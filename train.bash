a5491066988b5ba0b6e92467053d4916c8e67bec
zengwang <zengwang430521@gmail.com>
2021-04-09 09:51:03 +0800

    debug

--- train.bash
+++ train.bash
@@ -8,3 +8,6 @@ GPUS_PER_NODE=8 ./tools/run_dist_launch.sh 8 ./tools/expose.sh
 GPUS_PER_NODE=8 ./tools/run_dist_slurm.sh HA_3D expose 8 ./tools/expose.sh
 --exp-cfg data/conf_debug.yaml --output-dir OUTPUT_FOLDER --model-type simple --batch_size=8
 
+srun -p HA_3D --gres=gpu:8 -n1 --ntasks-per-node=1 --job-name=data_process --kill-on-bad-exit=1
+python generate_vertex --exp-cfg data/conf_debug.yaml  --exp-opts datasets.body.batch_size 1
+ --show=False --output-folder OUTPUT_FOLDER --save-params False --save-vis False --save-mesh False --model-type simple
