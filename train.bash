e4a940b082d43701c48e70b1aeb03ffbe00fec08
zengwang <zengwang430521@gmail.com>
2021-04-13 16:26:20 +0800

    mix dataset

--- train.bash
+++ train.bash
@@ -2,8 +2,6 @@ srun -p HA_3D --gres=gpu:8 -n1 --ntasks-per-node=1 --job-name=expose --kill-on-b
 python -u main.py --exp-cfg data/conf_debug.yaml --output-dir OUTPUT_FOLDER --model-type simple --batch_size=8
 
 
-GPUS_PER_NODE=8 ./tools/run_dist_launch.sh 8 ./tools/expose.sh
---exp-cfg data/conf_debug.yaml --output-dir OUTPUT_FOLDER --model-type simple --batch_size=8
 
 GPUS_PER_NODE=8 ./tools/run_dist_slurm.sh HA_3D expose 8 ./tools/expose.sh
 --exp-cfg data/conf_debug.yaml --output-dir OUTPUT_FOLDER --model-type simple --batch_size=8
